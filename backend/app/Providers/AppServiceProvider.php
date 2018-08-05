0df0cd9573434c0a24e815633414704c40a2ba8d
Gonzalo Ayuso <gonzalo123@gmail.com>
2018-08-05 19:37:51 +0200

    clean

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -12,24 +12,9 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        /*
-        $dbh = new PDO(getenv("DB_DSN"), getenv("DB_USER"), getenv("DB_PASS"));
-        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $dbh->setAttribute(PDO::ATTR_CASE, PDO::CASE_UPPER);
-        $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-        $dbal = DriverManager::getConnection(['pdo' => $dbh]);
-        $this->app->instance(Connection::class, $dbal);
-        */
     }
 
     public function register()
     {
-
-    }
-
-    private function registerLogger()
-    {
-
     }
 }
